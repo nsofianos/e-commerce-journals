3fada88aac57d6a19a38de8972e095c3550b53dd
Nik S <nsofianos@hotmail.com>
2022-09-28 20:13:10 -0700

    nav links selection script

--- public/scripts/scripts.js
+++ public/scripts/scripts.js
@@ -1,4 +1,4 @@
-window.onload = () => {
+const handleThumbnails = () => {
   const thumbnails = document.querySelectorAll(".thumbnail");
 
   thumbnails.forEach((thumbnail) => {
@@ -12,10 +12,21 @@ window.onload = () => {
       thumbnail.parentNode.classList.add("selected");
     });
   });
+};
 
-  // add script to html ->
-  //for each thumbnail ->
-  //addeventlistener click ->
-  //toggle off actives ->
-  //toggle on current active
+const handleNavigationTheme = () => {
+  const links = document.querySelectorAll(".nav-link");
+  links.forEach((link) => {
+    link.addEventListener("click", () => {
+      links.forEach((link) => {
+        link.classList.remove("selected");
+      });
+      link.classList.add("selected");
+    });
+  });
+};
+
+window.onload = () => {
+  handleThumbnails();
+  handleNavigationTheme();
 };
